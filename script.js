6b6bd06db65f0f2fad9106da40bac2e606233d37
akmaltrias <nazhwanursyabrina@gmail.com>
2024-03-05 16:41:38 +0700

    penambahan banner

--- script.js
+++ script.js
@@ -25,21 +25,3 @@ function showSlide(n) {
   
   slides[slideIndex].style.display = "block";  
 }
-
-//product slide//
-let productIndex = 0;
-    const productSlides = document.querySelectorAll('.product-slide');
-    showProductSlide();
-
-    function showProductSlide() {
-        for (let i = 0; i < productSlides.length; i++) {
-            productSlides[i].style.display = 'none';
-        }
-        productIndex++;
-        if (productIndex > productSlides.length) {
-            productIndex = 1;
-        }
-        productSlides[productIndex - 1].style.display = 'block';
-        setTimeout(showProductSlide, 5000); // Change slide every 5 seconds
-    };
-      
